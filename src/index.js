36721e22691138881fe07f5c28df8b96362a8835
Daniel <talonskullcustoms@gmail.com>
2022-07-15 19:32:38 -0400

    Set up hasura and restarted project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { useQuery, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+const CHARACTER = gql`
+	query getCharacters {
+		character {
+			id
+			name
+		}
+	}
+`;
+
+function Characters({ charaSelected }) {
+	const { loading, error, data } = useQuery(CHARACTER);
+
+	if (loading) return 'Loading...';
+	if (error) return `Error! ${error.message}`;
+
+	return (
+		<select name="Character" onChange={charaSelected}>
+			{data.character.map((character) => (
+				<option key={character.id} value={character.breed}>
+					{character.breed}
+				</option>
+			))}
+		</select>
+	);
+}
+
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		uri: 'https://thirddrill.hasura.app/v1/graphql',
+	}),
+});
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<h1>Third Drill</h1>
+		<Characters />
+	</ApolloProvider>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals(console.log);
